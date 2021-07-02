28e2ada689a36b5a3828c8eb9ef898445fc602f4
HANISHA2008 <85788364+HANISHA2008@users.noreply.github.com>
2021-07-02 09:44:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);                                                                                           
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+}
+ 
+var apple_sprites = Math.round(random(1,2));
+
+if(frameCount % 80 == 0) {
+  if(apple_sprites === 1) {
+    
+    function createApples() {
+    apple = createSprite(random(50,350),40,10,10);
+    apple.addImage("apple");
+    apple.velocity = 1;
+    apple.lifeTime();
+  
+    }
+
+  }
+}
+
+
+
+
+
+
